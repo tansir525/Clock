5152a356c5bb2579801f3b3140531d8c67a55827
tansir525 <tansirorick525@gmail.com>
2021-09-08 15:40:38 +0800

    version 1

---
+++ app.js
@@ -0,0 +1,20 @@
+function realtimeClock() {
+  var rtClock = new Date();
+  var hours = rtClock.getHours();
+  var minutes = rtClock.getMinutes();
+  var seconds = rtClock.getSeconds();
+
+  //Add AM and PM Syystem
+  var amPm = hours < 12 ? "AM" : "PM";
+  //convart hours component to 12 hours formate
+  hours = hours > 12 ? hours - 12 : hours;
+
+  //padd the hours, minutes and seconds with leadings zeros
+  hours = ("0" + hours).slice(-2);
+  minutes = ("0" + minutes).slice(-2);
+  seconds = ("0" + seconds).slice(-2);
+  //display the clock
+  document.getElementById("clock").innerHTML =
+    hours + " : " + minutes + " : " + seconds + " " + amPm;
+  var t = setTimeout(realtimeClock, 500);
+}
